fb51ac0009717c0873544ecb8492a32a1c5298d3
Adraas <pickalov.artyom@yandex.ru>
2018-05-22 09:25:07 +0400

    Some optimization

--- src/main/java/ru/wkn/Launcher.java
+++ src/main/java/ru/wkn/Launcher.java
@@ -2,11 +2,9 @@ package ru.wkn;
 
 import ru.wkn.distributions.PoissonDistribution;
 
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.OutputStreamWriter;
-import java.io.PrintWriter;
+import java.io.*;
 import java.nio.charset.Charset;
+import java.util.Properties;
 
 public class Launcher {
 
@@ -14,8 +12,11 @@ public class Launcher {
         try (PrintWriter printWriter = new PrintWriter(
                 new OutputStreamWriter(
                         new FileOutputStream("new_data34.txt"),
-                        Charset.forName("UTF-8").newEncoder()))) {
-            PoissonDistribution poissonDistribution = new PoissonDistribution(printWriter);
+                        Charset.forName("UTF-8").newEncoder()));
+             InputStream inputStream = Launcher.class.getResourceAsStream("/parameters/parameters.properties")) {
+            Properties properties = new Properties();
+            properties.load(inputStream);
+            PoissonDistribution poissonDistribution = new PoissonDistribution(properties, printWriter);
             poissonDistribution.distribute();
         } catch (IOException e) {
             e.printStackTrace();
--- src/main/java/ru/wkn/distributions/PoissonDistribution.java
+++ src/main/java/ru/wkn/distributions/PoissonDistribution.java
@@ -2,12 +2,15 @@ package ru.wkn.distributions;
 
 import java.io.IOException;
 import java.io.Writer;
+import java.util.Properties;
 
 public class PoissonDistribution {
 
+    private Properties properties;
     private Writer writer;
 
-    public PoissonDistribution(Writer writer) {
+    public PoissonDistribution(Properties properties, Writer writer) {
+        this.properties = properties;
         this.writer = writer;
     }
 
@@ -22,7 +25,7 @@ public class PoissonDistribution {
     public void distribute() throws IOException {
         double alpha = 0;
         for (int i = 0; i < 100; i++) {
-            double lambda = 0.95;
+            double lambda = Double.parseDouble(properties.getProperty("lambda"));
             double exp = Math.exp(-lambda);
             double s = exp;
             int k = 0;
