c7e9a6f45657886fd5ecaed6f6d04a085c3c542d
Adraas <pickalov.artyom@yandex.ru>
2018-10-02 17:32:02 +0400

    Window was filled

--- view/src/main/java/ru/wkn/windows/controllers/DistributorWindowController.java
+++ view/src/main/java/ru/wkn/windows/controllers/DistributorWindowController.java
@@ -1,4 +1,36 @@
 package ru.wkn.windows.controllers;
 
+import javafx.fxml.FXML;
+import javafx.scene.chart.BarChart;
+import javafx.scene.control.Button;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.TextField;
+
 public class DistributorWindowController {
+
+    @FXML
+    private BarChart barChartDistributions;
+    @FXML
+    private MenuItem menuItemClose;
+    @FXML
+    private MenuItem menuItemAbout;
+    @FXML
+    private Button buttonCheckByQualityControl;
+    @FXML
+    private TextField textFieldSizeOfSelection;
+    @FXML
+    private Button buttonGenerate;
+
+    @FXML
+    private void clickOnMenuItemClose() {
+        System.exit(0);
+    }
+
+    @FXML
+    private void clickOnButtonCheckByQualityControl() {
+    }
+
+    @FXML
+    private void clickOnButtonGenerate() {
+    }
 }
